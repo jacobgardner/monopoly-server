c507bc47c64c5ab71db5755eec004c1c3a44bed3
jrhii <james.r.harter.ii@gmail.com>
2016-09-07 16:20:44 -0500

    Revert "Revert "minor changes""
    This reverts commit 3901fb754b32ad25db48d841642c7e9638173d8f.

--- src/dummyClient/dummyClient2.js
+++ src/dummyClient/dummyClient2.js
@@ -4,12 +4,13 @@ var socket = require('socket.io-client')('http://localhost:3000');
 socket.on('connect', function(){
   console.log('connected');
 //  socket.emit('rollDice');
+
   socket.on('sendBoardState', function(BoardState){
     console.log('boardState received');
-    fs.writeFileSync('clientBoardState.json', BoardState);
+    fs.writeFileSync('client2BoardState.json', BoardState);
   });
 
-  socket.emit('register', 'john', '9qArn');
+  socket.emit('register', 'Tim', 'q2p0O');
 
   socket.on('promptRoll', function(BoardState){
     console.log('roll prompted');
--- src/main.js
+++ src/main.js
@@ -6,27 +6,26 @@ const server = http.createServer();
 const io = socketio(server);
 const MonopolyGame = new Monopoly();
 
-io.on('connect', function(socket){
+io.on('connection', function(socket){
   //assign user, get id, etc
   console.log('connected');
 
   socket.on('register', function(name, regID){
     MonopolyGame.loadPlayer(name, regID, socket.id);
-    MonopolyGame.cleanBoard();//test
-    MonopolyGame.runTurn(MonopolyGame.PlayerArray[0]);//test
   });
 
   MonopolyGame.Emitter.on('promptRoll', function(ActivePlayer){
     socket.once('rollDice', function(){
+      console.log('stuff');
       MonopolyGame.Emitter.emit('rollDice');
     });
     console.log('awaiting dice roll');
-    console.log('rollprompt to ' + ActivePlayer.socketID);
-    socket.emit('promptRoll', MonopolyGame.boardState());
+    io.to(ActivePlayer.socketID).emit('promptRoll', MonopolyGame.boardState());
   });
 
   socket.on('getBoardState', function(){
-    socket.broadcast.to(socket.id).emit('sendBoardState', MonopolyGame.boardState());
+    console.log('sending boardState to ' + socket.id);
+    io.to(socket.id).emit('sendBoardState', MonopolyGame.boardState());
   });
 
   socket.on('initiateTrade', function(){});//figure out trading.  simple to-from, this-for-that request?  counter trading?  >2 user trading, send conditions of trade to all users for acceptance?
