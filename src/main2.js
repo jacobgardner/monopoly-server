cd9b24d3283e423644c6b5c447888e75487cd4ff
jrhii <james.r.harter.ii@gmail.com>
2016-10-09 18:27:32 -0500

    troubleshoot

--- src/main2.js
+++ src/main2.js
@@ -1,6 +1,23 @@
 import fs from 'fs';
+import Monopoly from './monopoly';
 
-class Property {
+const monopoly = new Monopoly();
+
+monopoly.loadPropertyArray();
+
+
+fs.writeFileSync('testPropArray.json', JSON.stringify(monopoly.propertyArray, null, 2));
+for (const property of monopoly.propertyArray) {
+    console.log(`owner of ${property.nameStr}, ${property.ownerID} is not null: ${property.ownerId !== null}`);
+
+    if (property.ownerId) {
+        console.log('false');
+    } else {
+        console.log('true');
+    }
+}
+
+/*class Property {
     constructor(object) {
         for (const key in object) {
             this[key] = object[key];
@@ -17,10 +34,11 @@ console.log(propArr[4].nameStr);
 
 /*propArr.forEach((prop) => {
     propertyArray.push(new Property(prop));
-});*/
+});
 
 for (const property of propArr) {
     propertyArray.push(new Property(property));
 }
 
 fs.writeFileSync('newProperties.json', JSON.stringify(propertyArray, null, 2));
+*/
